ab49e4e4b1c0b92aed50669663f81debf55a78a2
junhobaik <junhobaik@gmail.com>
2017-08-15 01:17:46 +0900

    Add temp logo

--- src/components/Main/Logo.js
+++ src/components/Main/Logo.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 
+
+import logoImg from '../../images/logo.png';
+
 class Logo extends Component {
     render() {
         return (
             <div className="logo">
-                <img src="" alt="LOGO_IMG"/>
+                <img src={logoImg} alt="LOGO_IMG"/>
             </div>
         );
     }
