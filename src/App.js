2471f3b353f079212f3d24754124970924377a31
junhobaik <junhobaik@gmail.com>
2017-08-14 22:43:11 +0900

    Add Ad, outline

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import './App.scss';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import Main from './routes/Main';
+import Ad from './components/Ad/Ad';
 
 class App extends Component {
-  render() {
-    return (
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Main}/>
-        </Switch>
-      </Router>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <div>
+                    <Switch>
+                        <Route exact path="/" component={Main}/>
+                    </Switch>
+                    <Ad/>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
