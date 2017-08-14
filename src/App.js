eda3b0c04fdcffb78d7fe9f87efc91a23503a554
junhobaik <junhobaik@gmail.com>
2017-08-14 21:37:23 +0900

    Add Main outline, Apply react-router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+import Main from './routes/Main';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-
-      </div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Main}/>
+        </Switch>
+      </Router>
     );
   }
 }
---
+++ src/routes/Main.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import Logo from '../components/Main/Logo';
+import Search from '../components/Main/Search';
+
+
+class Main extends Component {
+    render() {
+        return (
+            <div className="Main">
+                <Logo/>
+                <Search/>
+            </div>
+        );
+    }
+}
+
+export default Main;
