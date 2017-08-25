06c41e06f2bb7bb52771304084731b8a1bcc9310
junhobaik <junhobaik@gmail.com>
2017-08-25 23:59:20 +0900

    Modify webpack config, add proxy

--- config/webpackDevServer.config.js
+++ config/webpackDevServer.config.js
@@ -7,8 +7,8 @@ const paths = require('./paths');
 
 const protocol = process.env.HTTPS === 'true' ? 'https' : 'http';
 const host = process.env.HOST || '0.0.0.0';
-
 module.exports = function(proxy, allowedHost) {
+  proxy = {"*" : "http://localhost:4000"};
   return {
     // WebpackDevServer 2.4.3 introduced a security fix that prevents remote
     // websites from potentially accessing local content through DNS rebinding:
