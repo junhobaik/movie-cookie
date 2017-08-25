a27edb88ca2806c40757b45cc1be01e3f2cc3f54
junhobaik <junhobaik@gmail.com>
2017-08-26 00:28:10 +0900

    Modify Search, add form

--- src/components/Main/Search.js
+++ src/components/Main/Search.js
@@ -4,13 +4,23 @@ import { Input } from 'semantic-ui-react';
 
 
 class Search extends Component {
+
+    componentDidMount(){
+        document.querySelector('.search i').onclick = function() {
+            document.querySelector('.search form').submit();
+            return false;
+        };
+    }
+
     render() {
         return (
             <div className="search">
-                <Input
-                    icon={{ name: 'search', circular: true, link: true }}
-                    placeholder='Search...'
-                />
+                <form action="/search" method="get">
+                    <Input name="movie"
+                           icon={{ name: 'search', circular: true, link: true }}
+                           placeholder='Search...'
+                    />
+                </form>
             </div>
         );
     }
